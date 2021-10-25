d286ad48ab969ebce5a4a19810fc42d2737ed5ac
JimmyHuang3364 <open3364@hotmail.com>
2021-10-26 02:16:03 +0800

    add login.hbs & register.hbs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const accounts = require('./modules/accounts')
+const users = require('./modules/users')
 
+router.use('/users', users)
 router.use('/accounts', accounts)
 router.use('/', home)
 
