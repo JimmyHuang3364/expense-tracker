4f011486685fa73b22f0d25804ad665081e2bc40
JimmyHuang3364 <open3364@hotmail.com>
2021-10-28 06:00:09 +0800

    update routes name

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
-const accounts = require('./modules/accounts')
+const expenseTracker = require('./modules/expenseTracker')
 const users = require('./modules/users')
 
 router.use('/users', users)
-router.use('/accounts', accounts)
+router.use('/expenseTracker', expenseTracker)
 router.use('/', home)
 
 module.exports = router
